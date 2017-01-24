bacb4d9de63c7572ef1d95bc982865018ae8e6c3
Aleksey Alkhutov <alexey.alkhutov@dunice.ru>
2017-01-24 15:52:12 +0300

    update settings

--- mimicprint/settings.py
+++ mimicprint/settings.py
@@ -137,8 +137,6 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/oos/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
