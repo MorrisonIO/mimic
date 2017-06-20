d48ba771563e71b5bb084e22b5cc60ad9ac8e601
Alexey Alkhutov <alexey.alkhutov@dunice.ru>
2017-06-20 15:42:14 +0300

    add Part Name, Quantity Required, Description columns to orders admin list

--- orders/admin.py
+++ orders/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.db.models import Q
-from models import Order, InventoryHistory, OrderedItem, WorkNote
+from django.db.models import Q, CharField
+from .models import Order, InventoryHistory, OrderedItem, WorkNote
 from daterange_filter.filter import DateRangeFilter
+from products.models import Product
 
 from uploads.models import Upload
 
 class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status', ('due_date', DateRangeFilter), ('date', DateRangeFilter), 'org')
-    list_display = ('name', 'org', 'date', 'due_date', 'status', 'worknotes_links', 'po_number',
+    list_display = ('name', 'org', 'date', 'due_date', 'status', 'descriptions', 'part_numbers', 'quantity_required', 'worknotes_links', 'po_number',
                     'docket_link', 'printed_button', 'shipping_links', 'invnum_form')
     search_fields = ('name', 'invoice_number', 'status', 'org__name', 'po_number')
     raw_id_fields = ('ship_to',)
@@ -29,7 +30,34 @@ class OrderAdmin(admin.ModelAdmin):
             form.base_fields['additional_file'].queryset = form.base_fields['additional_file'] \
                                                                         .queryset \
                                                                         .filter( Q(pk=additional_file.id) | Q(user_name=username) )
+
         return form
+
+
+    def descriptions(self, obj):
+        products_descriptions = ''
+        ih_objs = InventoryHistory.objects.filter(order_id=obj.id)
+        if len(ih_objs):
+            products_descriptions = '\n'.join(Product.objects.get(id=el.product_id).description for el in ih_objs \
+                                            if Product.objects.get(id=el.product_id).description)
+        return products_descriptions
+
+
+    def part_numbers(self, obj):
+        products_pns = ''
+        ih_objs = InventoryHistory.objects.filter(order_id=obj.id)
+        if len(ih_objs):
+            products_pns = '\n'.join(Product.objects.get(id=el.product_id).part_number for el in ih_objs \
+                                            if Product.objects.get(id=el.product_id).part_number)
+        return products_pns
+
+
+    def quantity_required(self, obj):
+        total = 0
+        ih_objs = InventoryHistory.objects.filter(order_id=obj.id)
+        products_descriptions = sum(el.amount for el in ih_objs if el.amount)
+        return products_descriptions
+
 
 class InventoryHistoryAdmin(admin.ModelAdmin):
     list_display = ('product', 'link_to_order', 'date', 'notes')
