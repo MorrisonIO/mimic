64c9fbbb2809c245da365d0fafe717a9ff6811ef
Alexey Alkhutov <a.alkhutov@dunice.net>
2017-10-04 18:00:45 +0300

    fix email address

--- orders/views.py
+++ orders/views.py
@@ -875,16 +875,14 @@ def collect_daily_jobs(request):
         subject = 'Current jobs:'.format()
         body = template_html.render(context)
         body_html = template_html.render(context)
+
         mail_list = [
-            'ivanov <a.ivanov@dunice.net>',
+            'Shelby Flores <Shelby.Flores@MimicPrint.com>',
+            'Tyler <Tyler@MimicPrint.com>',
+            'Laura Ambrozic <Laura.Ambrozic@MimicPrint.com>',
+            'Prepress <Prepress@MimicPrint.com>',
+            'Daniel <Daniel@MimicPrint.com>',
         ]
-        # mail_list = [
-        #     'Shelby Flores <Shelby.Flores@MimicPrint.com>',
-        #     'Tyler <Tyler@MimicPrint.com>',
-        #     'Laura Ambrozic <Laura.Ambrozic@MimicPrint.com>',
-        #     'Prepress <Prepress@MimicPrint.com>',
-        #     'Daniel <Daniel@MimicPrint.com>',
-        # ]
         send_mail(subject, body, 'orders@mimicprint.com', mail_list, fail_silently=False, html_message=body_html)
 
         return HttpResponse('ok')
