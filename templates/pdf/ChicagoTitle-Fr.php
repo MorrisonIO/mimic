c511b5d597d5c190dffe366f744854bb855e7424
Alexey Alkhutov <a.alkhutov@dunice.net>
2017-10-24 15:05:37 +0300

    add 3 templates

---
+++ templates/pdf/ChicagoTitle-Fr.php
@@ -0,0 +1,282 @@
+
+<!DOCTYPE html>
+<!-- saved from url=(0052)http://13.58.114.203/media/templates/pdf/page_4.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>Card</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <style>
+        @font-face {
+            font-family: 'nexa_boldregular';
+            src: url('/media/brochure_staff/fonts/nexa_bold-webfont.woff2') format('woff2'),
+            url('/media/brochure_staff/fonts/nexa_bold-webfont.woff') format('woff');
+            /*            src: url('./fonts/nexa_bold-webfont.woff2') format('woff2'),
+                             url('./fonts/nexa_bold-webfont.woff') format('woff');*/
+            font-weight: normal;
+            font-style: normal;
+
+        }
+        @font-face {
+            font-family: 'nexa_lightregular';
+            src: url('/media/brochure_staff/fonts/nexa_light-webfont.woff2') format('woff2'),
+            url('/media/brochure_staff/fonts/nexa_light-webfont.woff') format('woff');
+            /*            src: url('./fonts/nexa_light-webfont.woff2') format('woff2'),
+                             url('./fonts/nexa_light-webfont.woff') format('woff');*/
+            font-weight: normal;
+            font-style: normal;
+
+        }
+        @font-face {
+            font-family: 'nexa_regularregular';
+            src: url('/media/brochure_staff/Michael-Ohler-FNF-assests/fonts/nexaregular-webfont.woff2') format('woff2'),
+            url('/media/brochure_staff/Michael-Ohler-FNF-assests/fonts/nexaregular-webfont.woff') format('woff');
+            /*            src: url('./fonts/nexaregular-webfont.woff2') format('woff2'),
+                             url('./fonts/nexaregular-webfont.woff') format('woff');*/
+            font-weight: normal;
+            font-style: normal;
+
+        }
+        @page {
+            size: portrait;
+            width: 88.9mm;
+            height: 50.8mm;
+            width: 3.75in;
+            height: 2.25in;
+            margin: 0;
+            padding: 0;
+        }
+        input, textarea {
+            border: none;
+        }
+        img {
+            width: 100%;
+            height: auto;
+        }
+        .template-content {
+            position: relative;
+            width: 88.9mm;
+            height: 50.8mm;
+            width: 3.75in;
+            height: 2.25in;
+            margin: 0;
+            padding: 0;
+        }
+        .flip-vertical {
+            -moz-transform: scaleY(-1);
+            -o-transform: scaleY(-1);
+            -webkit-transform: scaleY(-1);
+            transform: scaleY(-1);
+            filter: FlipV;
+            -ms-filter: "FlipV";
+        }
+
+        .flip-horizontal {
+            -moz-transform: scale(-1, 1);
+            -webkit-transform: scale(-1, 1);
+            -o-transform: scale(-1, 1);
+            transform: scale(-1, 1);
+            filter: FlipH;
+            -ms-filter: "FlipH";
+        }
+
+        .template-page {
+            /*background: #117f46;*/
+            /*border: 1px solid #333;*/
+            width: 88.9mm;
+            height: 50.8mm;
+            width: 3.75in;
+            height: 2.25in;
+            position: relative;
+            page-break-after: always;
+        }
+        .TruePath-bg-back {
+
+        }
+        .bg-1 {
+            /*background-image: url('/media/brochure_staff/previews/ChicagoTitle-Fr-bleed.png');*/
+            /*background-image: url('./previews/ChicagoTitle-Fr-bleed.png');*/
+            background-image: none;
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        .background-image {
+            position: absolute;
+            top: 0mm;
+            left: 0mm;
+            width: 88.9mm;
+            height: 50.8mm;
+            width: 3.75in;
+            height: 2.25in;
+        img {
+            width: 88.9mm;
+            height: 50.8mm;
+            width: 3.75in;
+            height: 2.25in;
+        }
+        }
+        .logo-1 {
+            position: absolute;
+            top: 6.4mm;
+            width: 194px;
+            height: 41px;
+        img {
+            width: 194px;
+            height: 41px;
+        }
+        }
+        .logo-2 {
+            position: absolute;
+            top: 36.4mm;
+            right: 6.3mm;
+            width: 122px;
+            height: 31px;
+        img {
+            width: 122px;
+            height: 31px;
+        }
+        }
+
+        @media print {
+            html,
+            body {
+                size: portrait;
+                width: 88.9mm;
+                height: 50.8mm;
+                width: 3.75in;
+                height: 2.25in;
+                margin: 0;
+                padding: 0;
+            }
+            * {
+                box-sizing: border-box;
+                -moz-box-sizing: border-box;
+            }
+            .template-page {
+                size: landscape;
+                page-break-after: always;
+            }
+        }
+        .absolute {
+            position: absolute;
+        }
+        .left-align { left:10.3mm; }
+        .right-align { left: 54mm; }
+        .size-0 {
+            font-size: 6.2px;
+            margin: 0;
+        }
+        .size-1 {
+            font-size: 9.3px;
+            margin: 0;
+        }
+        .size-2 {
+            font-size: 10.5px;
+            margin: 0;
+        }
+        .color-1 {
+            color: #ab9055; /*gold*/
+        }
+        .color-2 {
+            color: #8b8a8a;/* grey */
+        }
+        .color-3 {
+            color: #000000;/* black */
+        }
+        .font-0 {
+            font-family: 'nexa_lightregular';
+            font-weight: normal;
+            font-style: normal;
+        }
+        .font-1 {
+            font-family: 'nexa_regularregular';
+            font-weight: normal;
+            font-style: normal;
+        }
+        .font-2 {
+            font-family: 'nexa_boldregular';
+            font-weight: normal;
+            font-style: normal;
+        }
+        .kind { margin-right: 1mm; }
+        .name { top:22.8mm; }
+        .position { top:26.2mm; }
+
+        .Address1 { top:24mm; }
+        .city { top: 26.6mm; }
+        .postal { top: 29.4mm; }
+        .social1 { top: 37.7mm; }
+        .social2 { top: 40.5mm; }
+        .social3 { top: 43mm; }
+        .social4 { top: 46mm; }
+
+        .cell { top:36.4mm; }
+        .office { top:33.6mm; }
+        .fax { top:37.7mm; }
+        .email { top:39.3mm; }
+        .web { top: 42mm; }
+        .placeholder:empty::before {
+            content: attr(data-placeholder);
+        }
+        span.kind {
+            display: inline-block;
+            width: 5px;
+        }
+        .fa {font-size: 8px;}
+
+    </style>
+</head>
+<body>
+    <div class="template-pages">
+        <div class="template-page TruePath-bg">
+
+            <div class="template-content">
+                <div class="background-image">
+                    <img class="background-image-img" alt="logo" src="/media/brochure_staff/logos/ChicagoTitle-fr-bg-bleed.svg" style="width: 3.75in; height: 2.25in; display: block;">
+                    <!-- <img class="background-image-img" alt="logo" src="./logos/ChicagoTitle-fr-bg-bleed.svg" style="width: 3.75in; height: 2.25in; display: block;"> -->
+                </div>
+                <div class="text-block">
+                        {% if not context %}
+
+                            <p class="absolute size-2 font-2 left-align color-1 placeholder name" data-placeholder="Full Name">Full Name</p>
+                            <p class="absolute size-1 font-1 left-align color-2 placeholder txt-upper position" data-placeholder="Position">Position</p>
+
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-upper Address1" data-placeholder="Address">Address</p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-upper city" data-placeholder="City, Province">City, Province</p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-upper postal" data-placeholder="Postal">Postal</p>
+
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-15 office" data-placeholder="Bureau"> <span class="kind color-3">(bureau)</span></p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-15 cell" data-placeholder="Cellulaire"> <span class="kind color-3">(cellulaire)</span></p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-15 email" data-placeholder="Email"> (Email)</p>
+
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social1"><i class="fa fa-twitter"></i> @ctic_ca</p>
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social2"><i class="fa fa-globe"></i> www.chicagotitle.ca</p>
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social3"><i class="fa fa-linkedin"></i> chicago title insurance company canada</p>
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social4"><i class="fa fa-phone"></i> 1.888.868.4853</p>
+
+                        {% else %}
+
+                            <p class="absolute size-2 font-2 left-align color-1 placeholder name" data-placeholder="Full Name">{{context.full_name}}</p>
+                            <p class="absolute size-1 font-1 left-align color-2 placeholder txt-upper position" data-placeholder="Position">{{context.position}}</p>
+
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-upper Address1" data-placeholder="Address">{{context.address}}</p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-upper city" data-placeholder="City, Province">{{context.city}}, {{context.province}}</p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-upper postal" data-placeholder="Postal">{{context.postal}}</p>
+
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-15 office" data-placeholder="Bureau">{{context.bureau}} <span class="kind color-3">(bureau)</span></p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-15 cell" data-placeholder="Cellulaire">{{context.cellulaire}} <span class="kind color-3">(cellulaire)</span></p>
+                            <p class="absolute size-1 font-1 right-align color-3 placeholder txt-15 email" data-placeholder="Email">{{context.email}}</p>
+
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social1"><i class="fa fa-twitter"></i> @ctic_ca</p>
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social2"><i class="fa fa-globe"></i> www.chicagotitle.ca</p>
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social3"><i class="fa fa-linkedin"></i> chicago title insurance company canada</p>
+                            <p class="absolute size-0 font-1 left-align color-2 placeholder txt-15 social4"><i class="fa fa-phone"></i> 1.888.868.4853</p>
+
+                        {% endif %}
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body></html>
