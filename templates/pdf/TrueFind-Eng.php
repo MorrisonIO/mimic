d3dc8dc57a284d296930ddecde6ebf54b968778f
Alexey Alkhutov <a.alkhutov@dunice.net>
2017-10-24 17:06:43 +0300

    add 3 templates

---
+++ templates/pdf/TrueFind-Eng.php
@@ -0,0 +1,288 @@
+
+<!DOCTYPE html>
+<!-- saved from url=(0052)http://13.58.114.203/media/templates/pdf/page_4.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>TrueFind</title>
+    <style>
+        @font-face {
+            font-family: 'nexa_boldregular';
+            src: url('/media/brochure_staff/fonts/nexa_bold-webfont.woff2') format('woff2'),
+                 url('/media/brochure_staff/fonts/nexa_bold-webfont.woff') format('woff');
+/*            src: url('./fonts/nexa_bold-webfont.woff2') format('woff2'),
+                 url('./fonts/nexa_bold-webfont.woff') format('woff');*/
+            font-weight: normal;
+            font-style: normal;
+
+        }
+        @font-face {
+            font-family: 'nexa_lightregular';
+            src: url('/media/brochure_staff/fonts/nexa_light-webfont.woff2') format('woff2'),
+                 url('/media/brochure_staff/fonts/nexa_light-webfont.woff') format('woff');
+/*            src: url('./fonts/nexa_light-webfont.woff2') format('woff2'),
+                 url('./fonts/nexa_light-webfont.woff') format('woff');*/
+            font-weight: normal;
+            font-style: normal;
+
+        }
+        @font-face {
+            font-family: 'nexa_regularregular';
+            src: url('/media/brochure_staff/Michael-Ohler-FNF-assests/fonts/nexaregular-webfont.woff2') format('woff2'),
+                 url('/media/brochure_staff/Michael-Ohler-FNF-assests/fonts/nexaregular-webfont.woff') format('woff');
+/*            src: url('./fonts/nexaregular-webfont.woff2') format('woff2'),
+                 url('./fonts/nexaregular-webfont.woff') format('woff');*/
+            font-weight: normal;
+            font-style: normal;
+
+        }
+        @page {
+            size: portrait;
+            width: 3.75in;
+            height: 2.25in;
+            display: block;
+            margin: 0;
+            padding: 0;
+        }
+        input, textarea {
+            border: none;
+        }
+        img {
+            width: 100%;
+            height: auto;
+        }
+        .template-content {
+            position: relative;
+            width: 3.75in;
+            height: 2.25in;
+            display: block;
+            margin: 0;
+            padding: 0;
+        }
+        .flip-vertical {
+            -moz-transform: scaleY(-1);
+            -o-transform: scaleY(-1);
+            -webkit-transform: scaleY(-1);
+            transform: scaleY(-1);
+            filter: FlipV;
+            -ms-filter: "FlipV";
+        }
+        
+        .flip-horizontal {
+            -moz-transform: scale(-1, 1);
+            -webkit-transform: scale(-1, 1);
+            -o-transform: scale(-1, 1);
+            transform: scale(-1, 1);
+            filter: FlipH;
+            -ms-filter: "FlipH";
+        }
+        
+        .template-page {
+            /*background: #117f46;*/
+            /*border: 1px solid #333;*/
+            width: 3.75in;
+            height: 2.25in;
+            display: block;
+            position: relative;
+            page-break-after: always;
+        }
+        .Truefind-bg-back {
+
+        }
+        .Truefind-bg {
+            /* background-image: url('/media/brochure_staff/Claudine-Rousse-FNF-assests/images/Claudine-Rousse-FNF--bg.jpg'); */
+            /*background-image: url('./previews/Truefind-Eng.jpg'); */
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        .background-image {
+            position: absolute;
+            top: 0mm;
+            left: 0mm;
+            width: 3.75in;
+            height: 2.25in;
+            display: block;
+            img {
+                width: 3.75in;
+                height: 2.25in;
+                display: block;
+            }
+        }
+        .logo-1 {
+            position: absolute;
+            top: 6.4mm;
+            width: 194px;
+            height: 41px;
+            img {
+                width: 194px;
+                height: 41px;
+            }
+        }
+        .logo-2 {
+            position: absolute;
+            top: 36.4mm;
+            right: 6.3mm;
+            width: 122px;
+            height: 31px;
+            img {
+                width: 122px;
+                height: 31px;
+            }
+        }
+        
+        @media print {
+            html,
+            body {
+                size: portrait;
+                width: 88.9mm;
+                height: 50.8mm;
+                margin: 0;
+                padding: 0;
+            }
+            * {
+                box-sizing: border-box;
+                -moz-box-sizing: border-box;
+            }
+            .template-page {
+                size: landscape;
+                page-break-after: always;
+            }
+        }
+        .absolute {
+            position: absolute;
+        }
+        .left-align {
+            left:19mm;
+        }
+        .right-align {
+            left: 44.5mm;
+        }
+        .size-0 {
+            font-size: 13.5px;
+            margin: 0;
+        }
+        .size-1 {
+            font-size: 12px;
+            margin: 0;
+        }
+        .size-2 {
+            font-size: 9px;
+            margin: 0;
+        }
+        .color-1 {
+            color: #177BC0; /*blue*/ 
+        }
+        .color-2 {
+            color: #177BC0;/* blue */
+        }
+        .color-3 {
+            color: #177BC0;
+        }
+        .font-0 {
+            font-family: 'nexa_lightregular';
+            font-weight: normal;
+            font-style: normal;
+        }
+        .font-1 {
+            font-family: 'nexa_regularregular';
+            font-weight: normal;
+            font-style: normal;
+        }
+        .font-2 {
+            font-family: 'nexa_boldregular';
+            font-weight: normal;
+            font-style: normal;
+        }
+        .kind {
+            margin-right: 1mm;
+        }
+        .name {
+            top:20mm;
+        }
+        .position {
+            top:25.1mm;
+        }
+        .Address {
+            top:46mm;
+        }
+        .cell {
+            top:35.7mm;
+        }
+        .office {
+            top:31.9mm;
+        }
+        .fax {
+            top:37.7mm;
+        }
+        .email {
+            top:39.6mm;
+        }
+        .web {
+            top: 42mm;
+        }
+        .placeholder:empty::before {
+            content: attr(data-placeholder);
+        }
+    span.kind {
+            display: inline-block;
+            width: 5px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="template-pages">
+        <div class="template-page Truefind-bg">
+
+            <div class="template-content">
+                <div class="background-image">
+                    <img class="background-image-img" alt="logo" src="/media/brochure_staff/logos/Truefind-bg.svg" style="width: 3.75in; height: 2.25in; display: block;">
+<!--                     <img class="background-image-img" alt="logo" src="./logos/Truefind-bg.svg" style="width: 3.75in; height: 2.25in; display: block;">-->
+                </div>
+                <div class="text-block">
+                        {% if not context %}
+                            <p class="absolute size-0 font-2 left-align color-1 placeholder name" data-placeholder="Full Name">Full Name</p>
+                            <p class="absolute size-2 font-1 left-align color-2 placeholder txt-upper position" data-placeholder="Position">Position</p>
+
+                            <p class="absolute size-1 font-0 left-align color-3 placeholder txt-15 office" data-placeholder="Office Phone"><span class="kind font-2 color-1">t: </span>Office Phone</p>
+                            <p class="absolute size-1 font-0 left-align color-3 placeholder txt-15 cell" data-placeholder="Cell"><span class="kind font-2 color-1">c: </span>Cell</p>
+                            <p class="absolute size-1 font-0 left-align color-3 placeholder txt-15 email" data-placeholder="Email"><span class="kind font-2 color-1">e: </span>Email</p>
+
+                            <p class="absolute size-2 font-0 left-align color-2 placeholder txt-upper Address" data-placeholder="Address">Address</p>
+
+
+
+
+                        {% else %}
+
+                            <p class="absolute size-0 font-2 left-align color-1 placeholder name" data-placeholder="Full Name">{{context.full_name}}</p>
+
+                            <p class="absolute size-2 font-1 left-align color-2 placeholder txt-upper position" data-placeholder="Position">{{context.position}}</p>
+
+
+                            <p class="absolute size-1 font-1 left-align color-3 placeholder txt-15 office" data-placeholder="Office Phone"><span class="kind font-2 color-1">t: </span> {{context.office_phone}}</p>
+                            <p class="absolute size-1 font-1 left-align color-3 placeholder txt-15 cell" data-placeholder="Cell"><span class="kind font-2 color-1">c: </span> {{context.cell}} </p>
+                            <p class="absolute size-1 font-1 left-align color-3 placeholder txt-15 email" data-placeholder="Email"><span class="kind font-2 color-1">e: </span> {{context.email}}</p>
+
+                            <p class="absolute size-2 font-1 left-align color-2 placeholder txt-upper Address" data-placeholder="Address">{{context.address}}</p>
+
+                        {% endif %}
+                </div>
+            </div>
+        </div>
+
+
+        <div class="template-page Truefind-bg-back">
+
+            <div class="template-content">
+                <div class="back-of-card full-screen">
+                    <img class="back-of-card-x" alt="Back" src="/media/brochure_staff/logos/Truefind-back-bg.svg" style="width: 3.75in; height: 2.25in; display: block;">
+<!--                     <img class="temp-img" alt="logo" src="./logos/Truefind-back-bg.svg" style="width: 3.75in; height: 2.25in; display: block;">-->
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body></html>
